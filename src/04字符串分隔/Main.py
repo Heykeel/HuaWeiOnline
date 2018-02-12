7f56ef0d119e3cd01ec01cd26e5e8ea9bdbd95a3
lijing <lijing@lijing-Lenovo>
2018-02-12 16:15:47 +0800

    上传时间：2018年02月12日16:15

--- src/04字符串分隔/Main.py
+++ src/04字符串分隔/Main.py
@@ -1,6 +1,14 @@
 import sys
 
-str1 = sys.stdin.readline( ).strip('\n')
+inStr1 = sys.stdin.readline( ).strip('\n')
+inStr2 = sys.stdin.readline( ).strip('\n')
 
-print(str1)
-
+inStrAll = [inStr1,inStr2]
+for m in range(2):
+    inStr = inStrAll[m]
+    inStrMat = [inStr[i:i+8] for i in range(0,len(inStr),8)]
+    
+    for strCell in inStrMat:
+        if(len(strCell)!=8):
+            strCell = strCell+"0"*(8-len(strCell))
+        print(strCell)
